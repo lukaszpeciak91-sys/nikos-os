10f7cf866b67cea503de3e94d8b35b4d4e70013e
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-09-21 12:48:43 +0200

    Define variable-length Communicator protocol v2

--- components/communicator_protocol/include/communicator_protocol/communicator_protocol.hpp
+++ components/communicator_protocol/include/communicator_protocol/communicator_protocol.hpp
@@ -5,8 +5,14 @@
 
 namespace nikos::communicator_protocol {
 
-constexpr std::uint8_t kVersion = 1;
-constexpr std::size_t kWireSize = 20;
+constexpr std::uint8_t kVersion = 2;
+constexpr std::size_t kHeaderSize = 2;
+constexpr std::size_t kPresenceWireSize = 2;
+constexpr std::size_t kRingWireSize = 6;
+constexpr std::size_t kAckWireSize = 6;
+constexpr std::size_t kPresetMessageWireSize = 7;
+constexpr std::size_t kPresetResponseWireSize = 11;
+constexpr std::size_t kMaxWireSize = kPresetResponseWireSize;
 
 enum class MessageType : std::uint8_t {
     Presence = 1,
@@ -23,7 +29,11 @@ struct Message {
     std::uint16_t value_id = 0;
 };
 
-bool encode(const Message& message, std::uint8_t* output, std::size_t output_size);
+bool encode(
+    const Message& message,
+    std::uint8_t* output,
+    std::size_t capacity,
+    std::size_t& encoded_length);
 bool decode(const std::uint8_t* data, std::size_t length, Message& message);
 
 }  // namespace nikos::communicator_protocol
