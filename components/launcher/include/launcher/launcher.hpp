19abb2f2af5c64d36649f5d23a4e79d97b1735df
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-09-22 19:44:10 +0200

    Add forced Launcher battery refresh option

--- components/launcher/include/launcher/launcher.hpp
+++ components/launcher/include/launcher/launcher.hpp
@@ -72,7 +72,7 @@ private:
         ShutdownConfirm,
     };
 
-    void update_battery_sample(std::uint32_t now_ms);
+    void update_battery_sample(std::uint32_t now_ms, bool force = false);
     void update_clock_sample(std::uint32_t now_ms, bool force = false);
     void reset_stopwatch_session();
     void exit_stopwatch_to_clock();
