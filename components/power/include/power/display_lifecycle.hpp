e0f551ec7cca38e6280b5d195da9b647764fced3
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-09-22 07:52:02 +0200

    Let display lifecycle own inactivity clock

--- components/power/include/power/display_lifecycle.hpp
+++ components/power/include/power/display_lifecycle.hpp
@@ -16,18 +16,18 @@ class DisplayLifecycle final {
 public:
     explicit DisplayLifecycle(board::Board& board);
 
-    void begin(std::uint32_t now_ms);
-    void update(std::uint32_t now_ms);
+    void begin();
+    void update();
 
-    board::InputState filter_input(
-        const board::InputState& input,
-        std::uint32_t now_ms);
+    board::InputState filter_input(const board::InputState& input);
 
-    void note_visible_activity(std::uint32_t now_ms);
+    void note_visible_activity();
 
     DisplayState state() const;
 
 private:
+    static std::uint32_t now_ms();
+
     void enter_active(std::uint32_t now_ms);
     void enter_dimmed();
     void enter_display_off();
