dace931f85b727ccddacaa0666af2ce6ecdba4d1
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-09-24 12:32:22 +0200

    Centralize display brightness profiles

--- components/settings/settings.cpp
+++ components/settings/settings.cpp
@@ -1,1 +1,18 @@
 #include "settings/settings.hpp"
+
+namespace nikos::settings {
+
+BrightnessProfile brightness_profile(Brightness brightness)
+{
+    switch (brightness) {
+        case Brightness::Low:
+            return BrightnessProfile{72, 18};
+        case Brightness::High:
+            return BrightnessProfile{128, 32};
+        case Brightness::Medium:
+        default:
+            return BrightnessProfile{96, 24};
+    }
+}
+
+}  // namespace nikos::settings
